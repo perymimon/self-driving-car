d04c1df80f2f7efdede469219eedecf2b5a2c7f3
pery mimon <pery.mimon@gmail.com>
2024-10-25 20:40:02 +0300

    add new tool 'onElementResize' in codeflow-util.js

--- js/utils/codeflow-utils.js
+++ js/utils/codeflow-utils.js
@@ -23,7 +23,7 @@ export function getMaxItem(array, valueFn) {
     var bestValue = valueFn(array.at(0))
     return array.reduce((bestItem, item) => {
         let value = valueFn(item)
-        if (value - bestValue <= 0 ) return bestItem
+        if (value - bestValue <= 0) return bestItem
         bestValue = value
         return item
     })
@@ -33,30 +33,86 @@ export function getMinItem(array, valueFn) {
     var bestValue = valueFn(array.at(0))
     return array.reduce((bestItem, item) => {
         let value = valueFn(item)
-        if (bestValue - value <= 0 ) return bestItem
+        if (bestValue - value <= 0) return bestItem
         bestValue = value
         return item
     })
 }
 
-export class Counter{
+export class Counter {
     constructor(count) {
         this.steps = count
         this.reset()
     }
-    counting(){
+
+    counting() {
         this.currentCount -= 1
         return this.currentCount <= 0
     }
-    countingIf(condition){
-        if(condition)
+
+    countingIf(condition) {
+        if (condition)
             return this.counting()
         else
             return this.reset()
     }
-    reset(){
+
+    reset() {
         this.currentCount = this.steps
         return false
     }
 
+}
+
+export function downloadJSON(json, filename = 'default.json') {
+    let a = document.createElement('a')
+    a.setAttribute('href', `data:application/json;charset=utf-8,${encodeURIComponent(JSON.stringify(json))}`)
+    a.setAttribute('download', filename)
+    a.click()
+}
+
+export async function readJsonFile(file, memoName) {
+    var {promise, resolve, reject} = Promise.withResolvers()
+    localStorage.setItem(memoName, file.name)
+    if (!file) {
+        reject('No File selected')
+        alert('No File selected')
+        return
+    }
+    let reader = new FileReader()
+    reader.onload = (evt) => {
+        let fileContent = evt.target.result
+        let jsonData = JSON.parse(fileContent)
+
+        resolve(jsonData)
+
+    }
+    reader.readAsText(file)
+    return promise
+}
+
+export async function fetchJSONFile(fileName) {
+    var res = await fetch(fileName)
+    return await res.json()
+}
+
+export async function fetchLastFile(memoName, defaultPath) {
+    var filename = localStorage.getItem(memoName);
+    if (filename)
+        return fetchJSONFile(filename)
+    else {
+        return fetchJSONFile(defaultPath)
+    }
+}
+
+export function onElementResize(element, callback) {
+    const resizeObserver = new ResizeObserver(entries => {
+        for (let entry of entries) {
+            callback(entry.contentRect, entry.target);
+        }
+    });
+    var rect = element.getBoundingClientRect()
+    callback(rect, element);
+    resizeObserver.observe(element);
+    return resizeObserver; // Return observer in case you want to disconnect later
 }
